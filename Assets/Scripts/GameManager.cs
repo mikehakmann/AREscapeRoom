77a360253ae7291c72d1b4cd87a3e56406b9dc95
mikehakmann <mikehakholm@gmail.com>
2024-11-18 13:35:46 +0100

    more stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,20 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    Intro,
+    WeightRiddle,
+    HandsensorRiddle
+}
+
 public class GameManager : MonoBehaviour
 {
+    private NetworkManager networkManager;
+    public GameState gameState = GameState.Intro;
 
-    NetworkManager networkManager;
+    public GameObject IntroObjects;
+    public GameObject WeightRiddle;
+    public GameObject HandsensorRiddle;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         networkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (gameState == GameState.Intro)
+        {
+            // Check if the player has clicked on the start button on the UI
+            // If so, change the gameState to WeightRiddle
+            if (IntroObjects.activeSelf == false)
+            {
+                IntroObjects.SetActive(true);
+                networkManager.SendData("{\"Intro\": true}");
+            }
+        }
+        else if (gameState == GameState.WeightRiddle)
+        {
+            // Check if the player has solved the weight riddle
+            // If so, change the gameState to HandsensorRiddle
+
+            if (WeightRiddle.activeSelf == false)
+            {
+                IntroObjects.SetActive(false);
+                WeightRiddle.SetActive(true);
+                networkManager.SendData("{\"WeightRiddle\": true}");
+            }
+        }
+        else if (gameState == GameState.HandsensorRiddle)
+        {
+            // Check if the player has solved the handsensor riddle
+            // If so, change the gameState to Intro
+            if (HandsensorRiddle.activeSelf == false)
+            {
+                WeightRiddle.SetActive(false);
+                HandsensorRiddle.SetActive(true);
+                networkManager.SendData("{\"HandsensorRiddle\": true}");
+            }
+        }
     }
 
     public void EndTask(int taskNumber)
@@ -29,7 +73,4 @@ public class GameManager : MonoBehaviour
     {
         networkManager.SendData("{\"quit\": true}");
     }
-    
-
-
 }
